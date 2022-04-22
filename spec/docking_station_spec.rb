c9e3d23cc81baec41b10419d55f9e03c3f656748
Sam Button <sam_button_94@hotmail.co.uk>
2022-04-22 11:11:18 +0100

    Co-authored-by: Farzan <Farzan-I@users.noreply.github.com> Co-authored-by: Kieran Carty <K-Carty@users.noreply.github.com>

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,24 +3,18 @@ require 'bike'
 
 describe DockingStation do
   it 'docking station to release bike' do
-
       docking_station = DockingStation.new
-      
-    expect{docking_station.release_bike}.not_to raise_error
-  end
-  
-  it { is_expected.to respond_to :release_bike }
-  # assert
-  # return a bike
-
-
-    #  DockingStation specification that a) gets a bike, and then b) expects the bike to be working
+      expect{docking_station.release_bike}.not_to raise_error
+    end
     
-
+  it { is_expected.to respond_to :release_bike }
+  
   it { is_expected.to respond_to :release_bike}
-
+  
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it {is_expected.to respond_to(:dock).with(1).argument }
 end
